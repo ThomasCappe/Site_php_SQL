ffdacbca5be6eefe5b46be3a83cb55d4cf0e37a5
Thomas Cappe <60007156+ThomasCappe@users.noreply.github.com>
2021-04-12 09:51:57 +0200

    Update TennisSql.php

--- TennisSql.php
+++ TennisSql.php
@@ -1,7 +1,3 @@
-<!DOCTYPE html>
-<html xmlns:th="http://www.thymeleaf.org"lang="fr">
-
-<head>
 	<?php
 
 			//Vérification et création d'un json avec toutes les reservations au mois ou au jour.
@@ -36,8 +32,8 @@
 							'mois' => $ligne['Mois'],
 							'jour' => $ligne['Jour'],
 							'Heure' => $ligne['Heure'],
-							'Id_Joueur1' => $ligne['Id_Joueur1'],
-							'Id_Joueur2' => $ligne['Id_Joueur2'],
+							'Id_Joueur1' => $ligne['Identifiant_1'],
+							'Id_Joueur2' => $ligne['Identifiant_2'],
 							'Terrain' => $ligne['Terrain'],
 						));
 					}
@@ -50,25 +46,26 @@
 			if ($_GET['Reservation']!=null){
 				$mysqli = new mysqli("localhost", "pi", "raspberry", "tennis");
 				$mysqli -> set_charset("utf8");
-				$requete = "INSERT INTO Reservation (Mois,Jour,Heure,Terrain,Id_Joueur1,Id_Joueur2) VALUES ('".$_GET['mois']."','".$_GET['jour']."','".$_GET['heure']."','".$_GET['terrain']."','".$_GET['id_joueur1']."','".$_GET['id_joueur2']."')";
+				$requete = "INSERT INTO Reservation (Mois,Jour,Heure,Terrain,Identifiant_1,Identifiant_2) VALUES ('".$_GET['mois']."','".$_GET['jour']."','".$_GET['heure']."','".$_GET['terrain']."','".$_GET['id_joueur1']."',".$_GET['id_joueur2'].")";
 				$resultat = $mysqli -> query($requete);
-				while ($ligne = $resultat -> fetch_assoc()) {
-					echo 'Envoi effectué';
-				}
+                                $response=array('Resusltat'=>true);
 				$mysqli->close();
+                                echo json_encode($response);
 			}
 
 			//Verification du mot de passe
 			if ($_GET['Identification']!=null){
 				$mysqli = new mysqli("localhost", "pi", "raspberry", "tennis");
 				$mysqli -> set_charset("utf8");
-				$requete = "SELECT MotDePasse FROM Utilisateurs WHERE Identifiant= '".$_GET['identifiant']."'";
+				$requete = "SELECT MotDePasse,Id,Admin FROM Utilisateurs WHERE Identifiant= '".$_GET['identifiant']."'";
 				$resultat = $mysqli -> query($requete);
-				$response = array();
+				$response = array('Verification'=>false);
 				while ($ligne = $resultat -> fetch_assoc()) {
 					if($ligne['MotDePasse']==$_GET['MotDePasse']){
 						$response=array(
 						'Verification' =>true,
+						'id' => $ligne['Id'],
+                                                'admin'=>$ligne['Admin']
 						);
 					}else{
 						$response=array(
@@ -79,8 +76,80 @@
 				$mysqli->close();
 				echo json_encode($response);
 			}
+                        //Liste des utilisateurs
+                        if ($_GET['Liste_Joueurs']!=null){
+                                $mysqli = new mysqli("localhost", "pi", "raspberry", "tennis");
+                                $mysqli -> set_charset("utf8");
+                                $requete = "SELECT Identifiant,Id FROM Utilisateurs ";
+                                $resultat = $mysqli -> query($requete);
+                                $response = array();
+                                while ($ligne = $resultat -> fetch_assoc()) {
+                                        array_push($response,array(
+                                        'Identifiant'=>$ligne['Identifiant'],
+                                        'Id'=>$ligne['Id'],
+                                        ));
+                                }
+                                $mysqli->close();
+                                echo json_encode($response);
+                        }
+                        //Ajout d'un utilisateur dans la BDD.
+                        if ($_GET['NewUtilisateur']!=null){
+                                $mysqli = new mysqli("localhost", "pi", "raspberry", "tennis");
+                                $mysqli -> set_charset("utf8");
+                                $requete = "INSERT INTO Utilisateurs (Prenom,Nom,Identifiant,DateDeNaissance,MotDePasse,Email,Tel) VALUES ('".$_GET['prenom']."','".$_GET['nom']."','".$_GET['identifiant']."','".$_GET['DateNaissance']."','".$_GET['motdepasse']."','".$_GET['mail']."',".$_GET['tel'].")";
+                                $resultat = $mysqli -> query($requete);
+                                while ($ligne = $resultat -> fetch_assoc()) {
+                                        echo 'Envoi effectué';
+                                }
+                                $mysqli->close();
+                        }
+                        //Demande de requete
+                        if ($_GET['Requete']!=null){
+                                $mysqli = new mysqli("localhost", "pi", "raspberry", "tennis");
+                                $mysqli -> set_charset("utf8");
+                                $requete = "UPDATE Utilisateurs SET Requete= ".$_GET['etatReq']." WHERE Id='".$_GET['Id']."'";
+                                $resultat = $mysqli -> query($requete);
+                                $mysqli->close();
+
+                                $mysqli = new mysqli("localhost", "pi", "raspberry", "tennis");
+                                $mysqli -> set_charset("utf8");
+                                $requete2 = "SELECT Code FROM Utilisateurs WHERE Id= '".$_GET['Id']."'";
+                                $resultat = $mysqli -> query($requete2);
+                                while($ligne=$resultat-> fetch_assoc()){
+                                        $response=array('Code'=>$ligne['Code']);
+                                }
+                                $mysqli->close();
+                                echo json_encode($response);
+                        }
+                        //Demande l'etat de l'acces 
+                        if ($_GET['Acces']!=null){
+                                $mysqli = new mysqli("localhost", "pi", "raspberry", "tennis");
+                                $mysqli -> set_charset("utf8");
+                                $requete = "UPDATE Utilisateurs SET Acces_Auth= ".$_GET['etatAcces']." WHERE Id='".$_GET['Id']."'";
+                                $resultat = $mysqli -> query($requete);
+                                $mysqli->close();
+                        }
+                        //Enregistrement du score
+                        if ($_GET['Rec_score']!=null){
+                                $mysqli = new mysqli("localhost", "pi", "raspberry", "tennis");
+                                $mysqli -> set_charset("utf8");
+                                $requete = "UPDATE Reservation SET Res_1='".$_GET['res']."' WHERE Identifiant_1='".$_GET['Identifiant_1']."'AND Identifiant_2='".$_GET['Identifiant_2']."'AND mois='".$_GET['mois']."'AND jour='".$_GET['jour']."'";
+                                echo($requete);
+                                $resultat = $mysqli -> query($requete);
+                                $mysqli->close();
+                        }
+                        //Lecture du score
+                        if ($_GET['ViewScore']!=null){
+                                $mysqli = new mysqli("localhost", "pi", "raspberry", "tennis");
+                                $mysqli -> set_charset("utf8");
+                                $requete = "SELECT Res_1 FROM Reservation WHERE Identifiant_1='".$_GET['Identifiant_1']."'AND Identifiant_2='".$_GET['Identifiant_2']."'AND mois='".$_GET['mois']."'AND jour='".$_GET['jour']."'";
+                                $resultat = $mysqli -> query($requete);
+                                while($ligne=$resultat-> fetch_assoc()){
+                                        $response=array('Resultat'=>$ligne['Res_1']);
+                                }
+                                $mysqli->close();
+                                echo json_encode($response);
+                        }
 			?>
-			</p>
-	</head>      
-</html>
+
 
